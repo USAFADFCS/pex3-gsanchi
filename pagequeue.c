ea1955965f7638a992bed0fbb59a734eed7f6bb1
gsanchi <g20fifi@gmail.com>
2026-04-20 23:03:36 -0600

    first two

--- pagequeue.c
+++ pagequeue.c
@@ -1,7 +1,7 @@
 /** pagequeue.c
  * ===========================================================
- * Name: _______________________, __ ___ 2026
- * Section: CS483 / ____
+ * Name: Gia Sanchirico, Apr 20 2026
+ * Section: CS483 / M3
  * Project: PEX3 - Page Replacement Simulator
  * Purpose: Implementation of the PageQueue ADT — a doubly-linked
  *          list for LRU page replacement.
@@ -18,7 +18,12 @@
 PageQueue *pqInit(unsigned int maxSize) {
     // TODO: malloc a PageQueue, set head and tail to NULL,
     //       size to 0, maxSize to maxSize, and return the pointer
-    return NULL;
+    PageQueue* pq = malloc(sizeof(PageQueue));
+    pq->head = NULL;
+    pq->tail = NULL;
+    pq->size = NULL;
+    pq->maxSize = maxSize;
+    return pq;
 }
 
 /**
@@ -27,17 +32,56 @@ PageQueue *pqInit(unsigned int maxSize) {
 long pqAccess(PageQueue *pq, unsigned long pageNum) {
     // TODO: Search the queue for pageNum (suggest searching tail->head
     //       so you naturally count depth from the MRU end).
-    //
-    // HIT path (page found at depth d):
-    //   - Remove the node from its current position and re-insert
-    //     it at the tail (most recently used).
-    //   - Return d.
-    //
-    // MISS path (page not found):
-    //   - Allocate a new node for pageNum and insert it at the tail.
-    //   - If size now exceeds maxSize, evict the head node (free it).
-    //   - Return -1.
-    return -1;
+
+    PqNode* tmp = pq->tail;
+    int i = 0;
+
+    while(tmp != NULL){
+        // HIT path (page found at depth d):
+        //   - Remove the node from its current position and re-insert
+        //     it at the tail (most recently used).
+        //   - Return d.
+        if(tmp->pageNum == pageNum){
+            //delete helper function?
+            free(tmp);git add
+            return i;
+        }
+        
+        // MISS path (page not found):
+        //   - Allocate a new node for pageNum and insert it at the tail.
+        //   - If size now exceeds maxSize, evict the head node (free it).
+        //   - Return -1.
+        else{
+            PqNode* newNode = pageNum;
+            newNode->next = NULL;
+            if(pq->head == NULL){
+                newNode->prev = NULL;
+                pq->head = newNode;
+                pq->tail = newNode;
+            }
+            else{
+                newNode->prev = pq->tail;
+                pq->tail->next = newNode;
+                pq->tail = newNode;
+            }
+
+            pq->size++;
+            if(pq->size > pq->maxSize){
+                //delete the head
+                pq->head = tmp->next;
+
+                pq->head->prev = NULL;
+                
+                pq->size--;
+
+                free(tmp);
+                return -1;
+            }
+
+        }
+        tmp = tmp->next;
+        i++;
+    }
 }
 
 /**
